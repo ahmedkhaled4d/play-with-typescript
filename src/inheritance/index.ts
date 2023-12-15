763181b52982faa3bf1fe5e5dd2773cf42faf4a6
ahmedkhaled4d <ahmedkhaled4d@gmail.com>
2023-12-15 22:27:56 +0200

    inheritance

---
+++ src/inheritance/index.ts
@@ -0,0 +1,39 @@
+class Animal {
+  constructor(public name: string) {}
+  move(distanceInMeters: number = 1) {
+    console.log(`${this.name} moved ${distanceInMeters}m.`);
+  }
+}
+
+class Snake extends Animal {
+  constructor(name: string) {
+    super(name);
+  }
+  move(distanceInMeters?: number): void {
+    console.log("Slithering...");
+    super.move(distanceInMeters);
+  }
+}
+
+class Horse extends Animal {
+  constructor(name: string) {
+    super(name);
+  }
+  move(distanceInMeters = 45) {
+    console.log("Galloping...");
+    super.move(distanceInMeters);
+  }
+}
+
+let sam = new Snake("Sammy the Python");
+let tom: Animal = new Horse("Tommy the Palomino");
+
+sam.move();
+tom.move(34);
+
+/**
+Slithering...
+Sammy the Python moved 1m.
+Galloping...
+Tommy the Palomino moved 34m.
+ */
