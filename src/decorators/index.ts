614072732db56a6247bd1619942c07b990a3d10b
ahmedkhaled4d <ahmedkhaled4d@gmail.com>
2023-12-15 22:49:13 +0200

    experimentalDecorators

---
+++ src/decorators/index.ts
@@ -0,0 +1,17 @@
+// Requires `experimentalDecorators` enabled
+
+@sealed
+class App {
+  greeting: string;
+  constructor(message: string) {
+    this.greeting = message;
+  }
+  greet() {
+    return "Hello, " + this.greeting;
+  }
+}
+
+function sealed(constructor: Function) {
+  Object.seal(constructor);
+  Object.seal(constructor.prototype);
+}
